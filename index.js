5293cf1b79ea64838fa60af000c00d738233725f
Lucia Belen Leiva <luciamccartney24@gmail.com>
2025-10-22 12:56:37 -0300

    Ultimos cambios

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const slides = document.querySelector('.carousel-slides');
+  const slide = document.querySelectorAll('.carousel-slide');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+  let index = 0;
 
-    const slides = document.querySelector('.carousel-slides');
-    const slide = document.querySelectorAll('.carousel-slide');
-    const prevBtn = document.querySelector('.prev');
-    const nextBtn = document.querySelector('.next');
-    let index = 0;
+  function showSlide(i) {
+    if (i < 0) index = slide.length - 1;
+    else if (i >= slide.length) index = 0;
+    else index = i;
+    slides.style.transform = `translateX(${-index * 100}%)`;
+  }
 
-    function showSlide(i) {
-      if (i < 0) index = slide.length - 1;
-      else if (i >= slide.length) index = 0;
-      else index = i;
-      slides.style.transform = `translateX(${-index * 100}%)`;
-    }
+  prevBtn.addEventListener('click', () => showSlide(index - 1));
+  nextBtn.addEventListener('click', () => showSlide(index + 1));
 
-    prevBtn.addEventListener('click', () => showSlide(index - 1));
-    nextBtn.addEventListener('click', () => showSlide(index + 1));
-
+  showSlide(index); // Mostrar el primer slide al cargar
+});
